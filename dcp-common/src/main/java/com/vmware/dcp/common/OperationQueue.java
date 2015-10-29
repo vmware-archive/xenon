4f515773c7d64d6e939cc7b17ac76537b9e94aa1
Georgios Chrysanthakopoulos <georgec@vmware.com>
2015-10-29 13:32:23 -0700

    Implement OperationQueue
    As part of the throttling / back pressure work, stateful service will
    use a limited capacity queue with evict capabilities. OperationQueue
    will be used instead of the concurrent linked queue in StatefulService.
    
    Advanced options (evict, LIFO) will be implemented in future changes
    
    Change-Id: I839f64f09dd2457cb2b6f284ce6457b357d6dba3

---
+++ dcp-common/src/main/java/com/vmware/dcp/common/OperationQueue.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.dcp.common;
+
+import java.util.ArrayDeque;
+import java.util.EnumSet;
+
+/**
+ * Queue implementation customized for the needs of a service. Depending on creation options
+ * it will act as a limited capacity {@code Deque} with either FIFO or LIFO behavior.
+ * The queue is not thread safe and should be sued within a synchronized context
+ */
+class OperationQueue {
+
+    public enum Option {
+        LIFO, FIFO, EVICT
+    }
+
+    public static OperationQueue create(int limit, EnumSet<Option> options) {
+        OperationQueue opDeque = new OperationQueue();
+        opDeque.limit = limit;
+        opDeque.options = options;
+        return opDeque;
+    }
+
+    private int limit;
+
+    private EnumSet<Option> options;
+
+    private ArrayDeque<Operation> store = new ArrayDeque<>(1);
+
+    private OperationQueue() {
+    }
+
+    public int getLimit() {
+        return this.limit;
+    }
+
+    EnumSet<Option> getOptions() {
+        return this.options;
+    }
+
+    /**
+     * Adds an element to the queue if the limit has not been reached.
+     * If the queue is configured to evict queued operations, the operation
+     * will be added and the either the newest or oldest queued operation will
+     * be failed with {@code CancellationException}
+     */
+    public boolean offer(Operation op) {
+        if (op == null) {
+            throw new IllegalArgumentException("op is required");
+        }
+
+        if (this.store.size() >= this.limit) {
+            return false;
+        }
+
+        return this.store.offer(op);
+    }
+
+    /**
+     * Retrieves and removes an operation. The operation is removed from the head of the
+     * queue if the queue is configured as FIFO, otherwise its removed from the tail
+     */
+    public Operation poll() {
+        return this.store.poll();
+    }
+
+}
---
+++ dcp-common/src/test/java/com/vmware/dcp/common/TestOperationQueue.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.dcp.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestOperationQueue {
+
+    public int count = 10000;
+
+    @Before
+    public void setUp() {
+        CommandLineArgumentParser.parseFromProperties(this);
+    }
+
+    @Test
+    public void offerAndPollWithDefaults() {
+        OperationQueue q = OperationQueue.create(this.count, null);
+
+        try {
+            q.offer(null);
+            throw new IllegalStateException("null offer should have failed");
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        assertEquals(this.count, q.getLimit());
+
+        List<Operation> ops = new ArrayList<>();
+
+        for (int i = 0; i < this.count; i++) {
+            Operation op = Operation.createPost(null);
+            ops.add(op);
+            q.offer(op);
+        }
+
+        // verify operations beyond limit are not queued
+        assertTrue(false == q.offer(Operation.createGet(null)));
+
+        // dequeue all operations, make sure they exist in our external list, in the expected order
+        for (Operation op : ops) {
+            Operation qOp = q.poll();
+            if (qOp.getId() != op.getId()) {
+                throw new IllegalStateException("unexpected operation from queue");
+            }
+        }
+
+        // verify no more operations remain
+        assertTrue(q.poll() == null);
+    }
+}
