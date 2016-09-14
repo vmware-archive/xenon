039f942b7b4499b32922fc0c1b2b48e9ccb77582
Vamsi Krishna Brahmajosyula <vbrahmajosyula@vmware.com>
2016-09-14 20:44:36 +0530

    Refractor Sync-Notification to Tx coordinator
    Consolidated into processCompletionStageTransactionNotification
    
    Change-Id: Iedba707f57b6c6c6f4ef1ff2900c61d224ee8738

--- xenon-common/src/main/java/com/vmware/xenon/common/StatefulService.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/StatefulService.java
@@ -685,20 +685,7 @@ public class StatefulService implements Service {
             }
             // operation has failed, complete and process any queued operations
             // If the request is in a transaction, notify the coordinator first.
-            final Throwable finalE = e;
-            if (op.isWithinTransaction() && this.getHost().getTransactionServiceUri() != null) {
-                allocatePendingTransactions();
-                notifyTransactionCoordinatorOp(this, op, e)
-                        .setCompletion((txOp, txE) -> {
-                            if (txE != null) {
-                                failRequest(op, txE);
-                                return;
-                            }
-                            failRequest(op, finalE);
-                        }).sendWith(this);
-                return;
-            }
-            failRequest(op, finalE);
+            processCompletionStageTransactionNotification(op, e);
             return;
         }
 
@@ -735,7 +722,7 @@ public class StatefulService implements Service {
                 // next stage will process pending operations, disable finally clause from
                 // duplicating work
                 processPending = false;
-                processCompletionStageIndexing(op, null);
+                processCompletionStageIndexing(op);
             } else {
                 processCompletionStagePublishAndComplete(op);
             }
@@ -934,7 +921,7 @@ public class StatefulService implements Service {
                 commitOp.addRequestHeader(Operation.REPLICATION_QUORUM_HEADER, replQuorum);
                 commitOp.setCompletion((o, e) -> {
                     // regardless of commit success, we need to proceed with operation processing
-                    processCompletionStageIndexing(op, null);
+                    processCompletionStageIndexing(op);
                 });
             }
 
@@ -946,7 +933,7 @@ public class StatefulService implements Service {
             if (!indexState) {
                 return;
             }
-            processCompletionStageIndexing(op, null);
+            processCompletionStageIndexing(op);
         }
     }
 
@@ -955,28 +942,14 @@ public class StatefulService implements Service {
      * and kicks of pending request processing. On indexing completion, it continues with next stage
      * of operation completion
      */
-    private void processCompletionStageIndexing(Operation op, Throwable e) {
+    private void processCompletionStageIndexing(Operation op) {
         try {
             op.nestCompletion((o, failure) -> {
                 if (failure != null) {
                     failRequest(op, failure);
                     return;
                 }
-
-                // Notify transaction coordinator before completing the operation
-                if (op.isWithinTransaction() && this.getHost().getTransactionServiceUri() != null) {
-                    allocatePendingTransactions();
-                    notifyTransactionCoordinatorOp(this, op, e)
-                            .setCompletion((txOp, txE) -> {
-                                if (txE != null) {
-                                    failRequest(op, txE);
-                                    return;
-                                }
-                                processCompletionStagePublishAndComplete(op);
-                            }).sendWith(this);
-                    return;
-                }
-                processCompletionStagePublishAndComplete(op);
+                processCompletionStageTransactionNotification(op, null);
             });
 
             ServiceDocument mergedState = op.getLinkedState();
@@ -984,6 +957,38 @@ public class StatefulService implements Service {
         } finally {
             processPending(op);
         }
+    }
+
+    /**
+     * Process notification to transaction coordinator and complete request
+     */
+    private void processCompletionStageTransactionNotification(Operation op, Throwable e) {
+        // Notify transaction coordinator before completing the operation
+        if (op.isWithinTransaction() && this.getHost().getTransactionServiceUri() != null) {
+            allocatePendingTransactions();
+            notifyTransactionCoordinatorOp(this, op, e)
+                    .setCompletion((txOp, txE) -> {
+                        if (txE != null) {
+                            failRequest(op, txE);
+                            return;
+                        }
+
+                        if (e == null) {
+                            processCompletionStagePublishAndComplete(op);
+                            return;
+                        }
+
+                        failRequest(op, e);
+                    }).sendWith(this);
+            return;
+        }
+
+        if (e == null) {
+            processCompletionStagePublishAndComplete(op);
+            return;
+        }
+
+        failRequest(op, e);
     }
 
     /**
