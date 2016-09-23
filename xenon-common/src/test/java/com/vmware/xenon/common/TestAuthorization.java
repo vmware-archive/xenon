530a287f351568e5e53fa0e73add506dacbadbee
Tadaya Tsuyukubo <ttsuyukubo@vmware.com>
2016-09-22 18:12:04 -0700

    Disable TestAuthorization#actionBasedAuthorization
    https://www.pivotaltracker.com/story/show/130803239
    
    Change-Id: Ic921e371141a39d2af5632101c7f7639ef3b3542

--- xenon-common/src/test/java/com/vmware/xenon/common/TestAuthorization.java
+++ xenon-common/src/test/java/com/vmware/xenon/common/TestAuthorization.java
@@ -38,6 +38,7 @@ import java.util.function.Consumer;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.vmware.xenon.common.Operation.AuthorizationContext;
@@ -308,6 +309,7 @@ public class TestAuthorization extends BasicTestCase {
     }
 
     @Test
+    @Ignore("https://www.pivotaltracker.com/story/show/130803239")
     public void actionBasedAuthorization() throws Throwable {
 
         // Assume Jane's identity
