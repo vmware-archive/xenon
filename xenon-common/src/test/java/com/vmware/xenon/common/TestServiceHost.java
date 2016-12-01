6e0709ce4e6c7329f2a66bf4e3d3cc1cf51fa9c7
George Chrysanthakopoulos <georgec@vmware.com>
2016-12-01 11:46:08 -0800

    Fix ODL on demand start/stop edge case
    This is a single failure of this test after about 30,000 runs...
    
    The issue is that cancellation of operations queued on a stateful service
    needs to really consider if the service is ODL, and retry. We already do
    this but not in all cases. I am sure there might more edge cases.
    
    https://www.pivotaltracker.com/story/show/135311923
    
    Change-Id: I4885450615e5fa41cff6fa86ba21355ad14ab26e

--- xenon-common/src/test/java/com/vmware/xenon/common/TestServiceHost.java
+++ xenon-common/src/test/java/com/vmware/xenon/common/TestServiceHost.java
@@ -1978,6 +1978,13 @@ public class TestServiceHost {
 
     @Test
     public void onDemandServiceStopCheckWithReadAndWriteAccess() throws Throwable {
+        for (int i = 0; i < this.iterationCount; i++) {
+            tearDown();
+            doOnDemandServiceStopCheckWithReadAndWriteAccess();
+        }
+    }
+
+    private void doOnDemandServiceStopCheckWithReadAndWriteAccess() throws Throwable {
         setUp(true);
 
         long maintenanceIntervalMicros = TimeUnit.MILLISECONDS.toMicros(100);
@@ -2099,7 +2106,7 @@ public class TestServiceHost {
         for (int i = 0; i < requestCount; i++) {
             patch = createMinimalTestServicePatch(servicePath, ctx);
             this.host.send(patch);
-            if (i == requestCount / 2) {
+            if (i == Math.min(10, requestCount / 2)) {
                 Operation deleteStop = Operation.createDelete(this.host, servicePath)
                         .addPragmaDirective(Operation.PRAGMA_DIRECTIVE_NO_INDEX_UPDATE);
                 this.host.send(deleteStop);
