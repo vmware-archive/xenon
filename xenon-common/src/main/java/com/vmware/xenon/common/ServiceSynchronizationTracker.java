f3d788894ec645b6415872587cfec03908dbaf66
sufiand <dars@vmware.com>
2016-12-09 11:59:29 -0800

    Set statusCode and failure body correctly for start operations
    We have observed a few failures where a start operation did not
    fail with the correct status code. This change explicitly sets
    the status code and failure body of the start operation on failure.
    
    Change-Id: Ib404f0c70b73d7675a4e81ad0e735f20c41fcfcf

--- xenon-common/src/main/java/com/vmware/xenon/common/ServiceSynchronizationTracker.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/ServiceSynchronizationTracker.java
@@ -139,7 +139,13 @@ class ServiceSynchronizationTracker {
                             Utils.fromNowMicrosUtc(
                                     NodeGroupService.PEER_REQUEST_TIMEOUT_MICROS))
                     .setCompletion((synchOp, t) -> {
-                        start.fail(startEx);
+                        if (t != null) {
+                            this.host.log(Level.SEVERE, "Synch failed for %s. Exception: %s",
+                                    service.getSelfLink(), t.toString());
+                        }
+                        // It's important that we fail the original POST request with the same
+                        // failure, statusCode and body.
+                        start.fail(startRsp.getStatusCode(), startEx, startRsp.getBodyRaw());
                         this.host.processPendingServiceAvailableOperations(
                                 service, startEx, !start.isFailureLoggingDisabled());
                     });
@@ -147,7 +153,7 @@ class ServiceSynchronizationTracker {
             return;
         }
 
-        start.fail(startEx);
+        start.fail(startRsp.getStatusCode(), startEx, startRsp.getBodyRaw());
         this.host.processPendingServiceAvailableOperations(
                 service, startEx, !start.isFailureLoggingDisabled());
     }
