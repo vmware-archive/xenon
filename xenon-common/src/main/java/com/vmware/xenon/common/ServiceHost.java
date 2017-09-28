c0e5fb8546b9cfd9d66cd5c44c3bd0a17c7c9420
Li Yu <lyu1@vmware.com>
2017-09-28 16:16:27 -0700

    Register service availability with node-selector link
    Currently if user did not provided node-selector link to
    registerForServiceAvailability, default node-selector used.
    Instead, configged node-selector should be used.
    
    https://www.pivotaltracker.com/story/show/150348484
    
    Change-Id: I3520c35aeccd615443321b333979436b8218e01c

--- xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
@@ -5071,6 +5071,10 @@ public class ServiceHost implements ServiceRequestSender {
             ch.handle(null, new IllegalStateException("service not found"));
             return;
         }
+        String peerNodeSelectorPath = s.getPeerNodeSelectorPath();
+        if (peerNodeSelectorPath != null && !peerNodeSelectorPath.equals(nodeSelectorPath)) {
+            nodeSelectorPath = peerNodeSelectorPath;
+        }
         NodeGroupUtils.checkServiceAvailability(ch, s.getHost(), s.getSelfLink(), nodeSelectorPath);
     }
 
