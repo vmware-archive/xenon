9be8d4f801644c73a190397ef9b8c6f3c45837c4
Julian Vassev <jvassev@vmware.com>
2017-10-23 23:01:56 +0300

    Smarter collection sizing
    Profiling revealed that some collection contain a single
    element almost all the time. This patch adjusts
    paginatedSearchersByExpirationTime's size to 1 (highly unlikely two tasks would
    expire in the same microsecond) and kindScope to 4 (very few queries have more
    than a 2 kinds).
    Default values are 10 and 16 respectively.
    
    Change-Id: I3fa6f9864e8894bf3848719e2133554c756e851e

--- xenon-common/src/main/java/com/vmware/xenon/services/common/LuceneDocumentIndexService.java
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/LuceneDocumentIndexService.java
@@ -174,6 +174,10 @@ public class LuceneDocumentIndexService extends StatelessService {
 
     private static int metadataUpdateMaxQueueDepth = DEFAULT_METADATA_UPDATE_MAX_QUEUE_DEPTH;
 
+    private final Runnable queryTaskHandler = this::handleQueryRequest;
+
+    private final Runnable updateRequestHandler = this::handleUpdateRequest;
+
     public static void setImplicitQueryResultLimit(int limit) {
         queryResultLimit = limit;
     }
@@ -375,6 +379,7 @@ public class LuceneDocumentIndexService extends StatelessService {
      * Searchers used for paginated query tasks.
      */
     protected TreeMap<Long, PaginatedSearcherInfo> paginatedSearchersByCreationTime = new TreeMap<>();
+
     protected TreeMap<Long, List<PaginatedSearcherInfo>> paginatedSearchersByExpirationTime = new TreeMap<>();
 
     protected IndexWriter writer = null;
@@ -931,11 +936,11 @@ public class LuceneDocumentIndexService extends StatelessService {
         try {
             if (a == Action.GET || a == Action.PATCH) {
                 if (offerQueryOperation(op)) {
-                    this.privateQueryExecutor.execute(this::handleQueryRequest);
+                    this.privateQueryExecutor.execute(this.queryTaskHandler);
                 }
             } else {
                 if (offerUpdateOperation(op)) {
-                    this.privateIndexingExecutor.execute(this::handleUpdateRequest);
+                    this.privateIndexingExecutor.execute(this.updateRequestHandler);
                 }
             }
         } catch (RejectedExecutionException e) {
@@ -968,7 +973,7 @@ public class LuceneDocumentIndexService extends StatelessService {
                     ServiceDocument sd = (ServiceDocument) op.getBodyRaw();
                     if (sd.documentKind != null) {
                         if (sd.documentKind.equals(QueryTask.KIND)) {
-                            QueryTask task = (QueryTask) op.getBodyRaw();
+                            QueryTask task = (QueryTask) sd;
                             handleQueryTaskPatch(op, task);
                             break;
                         }
@@ -1207,8 +1212,11 @@ public class LuceneDocumentIndexService extends StatelessService {
 
         info.expirationTimeMicros = newExpirationMicros;
 
+        // initialize the array with size = 1: unlikely that two searcher will expire
+        // at the same microsecond. The default size of 10 is almost never filled up.
         expirationList = this.paginatedSearchersByExpirationTime.computeIfAbsent(
-                newExpirationMicros, (k) -> new ArrayList<>());
+                newExpirationMicros, _k -> new ArrayList<>(1));
+
         expirationList.add(info);
 
         return info.searcher;
