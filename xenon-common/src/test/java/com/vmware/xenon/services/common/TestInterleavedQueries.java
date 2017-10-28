a9c38c78d551604858fd7f712bff0a430300d908
Julian Vassev <jvassev@vmware.com>
2017-10-28 16:00:08 +0300

    Split InterleaveQueries test
    Split in stress and throughput test.
    
    Change-Id: I9eff60032cedf5480d23e041e2118fcb479055a0

--- xenon-common/src/test/java/com/vmware/xenon/services/common/TestInterleavedQueries.java
+++ xenon-common/src/test/java/com/vmware/xenon/services/common/TestInterleavedQueries.java
@@ -15,17 +15,21 @@ package com.vmware.xenon.services.common;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import com.vmware.xenon.common.BasicReusableHostTestCase;
 import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.Service.ServiceOption;
+import com.vmware.xenon.common.ServiceConfigUpdateRequest;
 import com.vmware.xenon.common.TestResults;
 import com.vmware.xenon.common.UriUtils;
 import com.vmware.xenon.common.Utils;
@@ -41,17 +45,33 @@ public class TestInterleavedQueries extends BasicReusableHostTestCase {
     public int operationTimeoutMillis;
     public int errorThreshold;
 
+    @Before
+    public void setup() {
+        ServiceConfigUpdateRequest body = ServiceConfigUpdateRequest.create();
+        body.addOptions = EnumSet.of(ServiceOption.INSTRUMENTATION);
+
+        this.host.getTestRequestSender().sendAndWait(
+                Operation.createPatch(UriUtils.buildConfigUri(this.host, ServiceUriPaths.CORE_DOCUMENT_INDEX)).setBody(
+                        body
+                ));
+    }
+
     public TestInterleavedQueries() {
         // These values picked for IDE configuration: short test,
         // default heap size of 256m
 
-        this.requestCount = 200;
+        this.requestCount = 100;
         this.operationTimeoutMillis = 350;
         this.errorThreshold = 5;
     }
 
+    /**
+     * How many interleaved queries does it take for system to become unresponsive?
+     *
+     * @throws Throwable
+     */
     @Test
-    public void testInterleaved() throws Throwable {
+    public void testStress() throws Throwable {
         // assume there are 50 documentKinds each having 20 fields
         final int fieldCount = 50 * 20;
         List<String> keys = new ArrayList<>(fieldCount);
@@ -70,13 +90,10 @@ public class TestInterleavedQueries extends BasicReusableHostTestCase {
 
         long start = System.nanoTime();
 
-        // finish test after this many timeouts
-        // requests timeout because of GC pressure
-        int errorThreshold = this.errorThreshold;
-
         int i;
+        int errors = this.errorThreshold;
         for (i = 0; i < this.requestCount; i++) {
-            this.host.log("Processing document %s", i);
+            this.host.log("Processing request %s", i);
             ExampleServiceState doc = createBigState(keys);
             Operation postDocument = Operation.createPost(UriUtils.buildUri(this.host, ExampleService.FACTORY_LINK))
                     .setExpiration(Utils.fromNowMicrosUtc(this.operationTimeoutMillis * 1000))
@@ -85,25 +102,14 @@ public class TestInterleavedQueries extends BasicReusableHostTestCase {
             try {
                 this.host.getTestRequestSender().sendAndWait(postDocument);
             } catch (Throwable e) {
-                errorThreshold--;
-                if (errorThreshold == 0) {
-                    writeResults(start, i);
-                    return;
+                errors--;
+                if (errors == 0) {
+                    this.host.log("System become unresponsive after %d requests ", i);
+                    break;
                 }
             }
 
-            QueryTask task = QueryTask.Builder.createDirectTask()
-                    .setQuery(QueryTask.Query.Builder.create()
-                            .addKindFieldClause(ExampleServiceState.class)
-                            .addFieldClause(ExampleServiceState.FIELD_NAME_NAME, "a", MatchType.WILDCARD,
-                                    Occurance.MUST_NOT_OCCUR)
-                            .addFieldClause(ExampleServiceState.FIELD_NAME_NAME, "c", MatchType.WILDCARD,
-                                    Occurance.MUST_NOT_OCCUR)
-                            .addFieldClause(ExampleServiceState.FIELD_NAME_NAME, "5", MatchType.WILDCARD,
-                                    Occurance.MUST_NOT_OCCUR)
-                            .build())
-                    .setResultLimit(50)
-                    .build();
+            QueryTask task = makeQueryTask();
 
             // don't expire tasks anytime soon: expiring them before the test is over will skew the test
             task.documentExpirationTimeMicros = Utils.fromNowMicrosUtc(TimeUnit.MINUTES.toMicros(30));
@@ -112,25 +118,102 @@ public class TestInterleavedQueries extends BasicReusableHostTestCase {
                     .setBody(task);
             this.host.getTestRequestSender().sendAndWait(postTask);
         }
-        writeResults(start, i);
+
+        writeResults(start, i, this.errorThreshold);
     }
 
-    private void writeResults(long start, int opCount) {
-        double tput = opCount / TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start);
+    private QueryTask makeQueryTask() {
+        return QueryTask.Builder.createDirectTask()
+                .setQuery(QueryTask.Query.Builder.create()
+                        .addKindFieldClause(ExampleServiceState.class)
+                        .addFieldClause(ExampleServiceState.FIELD_NAME_NAME, "a", MatchType.WILDCARD,
+                                Occurance.MUST_NOT_OCCUR)
+                        .addFieldClause(ExampleServiceState.FIELD_NAME_NAME, "c", MatchType.WILDCARD,
+                                Occurance.MUST_NOT_OCCUR)
+                        .addFieldClause(ExampleServiceState.FIELD_NAME_NAME, "5", MatchType.WILDCARD,
+                                Occurance.MUST_NOT_OCCUR)
+                        .build())
+                .setResultLimit(50)
+                .build();
+    }
+
+    /**
+     * What's the throughput when processing N request under interleaved queries load?
+     *
+     * @throws Throwable
+     */
+    @Test
+    public void testThroughput() throws Throwable {
+        // assume there are 50 documentKinds each having 20 fields
+        final int fieldCount = 50 * 20;
+        List<String> keys = new ArrayList<>(fieldCount);
+        for (int i = 0; i < fieldCount; i++) {
+            keys.add("key-" + UUID.randomUUID());
+        }
+
+        // create some initial documents
+        for (int i = 0; i < 500; i++) {
+            ExampleServiceState doc = createBigState(keys);
+            Operation postDocument = Operation.createPost(UriUtils.buildUri(this.host, ExampleService.FACTORY_LINK))
+                    .setBody(doc);
+
+            this.host.getTestRequestSender().sendAndWait(postDocument);
+        }
+
+        long start = System.nanoTime();
+
+        int errors = 0;
+        int i;
+        for (i = 0; i < this.requestCount; i++) {
+            this.host.log("Processing request %s", i);
+            ExampleServiceState doc = createBigState(keys);
+            Operation postDocument = Operation.createPost(UriUtils.buildUri(this.host, ExampleService.FACTORY_LINK))
+                    .setExpiration(Utils.fromNowMicrosUtc(this.operationTimeoutMillis * 1000))
+                    .setBody(doc);
+
+            try {
+                this.host.getTestRequestSender().sendAndWait(postDocument);
+            } catch (Throwable e) {
+                errors++;
+                this.host.log("Operation timed out, retrying");
+                i--;
+                continue;
+            }
+
+            QueryTask task = makeQueryTask();
+
+            // don't expire tasks anytime soon: expiring them before the test is over will skew the test
+            task.documentExpirationTimeMicros = Utils.fromNowMicrosUtc(TimeUnit.MINUTES.toMicros(30));
+
+            Operation postTask = Operation.createPost(UriUtils.buildUri(this.host, ServiceUriPaths.CORE_QUERY_TASKS))
+                    .setBody(task);
+            this.host.getTestRequestSender().sendAndWait(postTask);
+        }
+
+        writeResults(start, i, errors);
+    }
+
+    private void writeResults(long start, int opCount, int errors) {
+        long duration = System.nanoTime() - start;
+        double tput = opCount / TimeUnit.NANOSECONDS.toSeconds(duration);
 
         this.testResults.getReport().lastValue("ops", opCount);
+        this.testResults.getReport().lastValue("time", duration / 1000_000_000.0);
+        this.testResults.getReport().lastValue("errors", errors);
         this.testResults.getReport().lastValue(TestResults.KEY_THROUGHPUT, tput);
+        this.host.logServiceStats(UriUtils.buildUri(
+                this.host, ServiceUriPaths.CORE_DOCUMENT_INDEX), this.testResults);
 
         this.host.log("throughput: %f ops/s, count = %d", tput, opCount);
-        this.host.log("Interleaving queries made system unusable after %s POSTs", opCount);
     }
 
     private ExampleServiceState createBigState(List<String> keys) {
         ExampleServiceState res = new ExampleServiceState();
         res.keyValues = new HashMap<>();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
         // populate some number of fields
         for (int i = 0; i < 20; i++) {
-            res.keyValues.put(keys.get(ThreadLocalRandom.current().nextInt(keys.size())), "" + new Date());
+            res.keyValues.put(keys.get(random.nextInt(keys.size())), "" + new Date());
         }
         res.counter = System.currentTimeMillis();
         res.name = UUID.randomUUID().toString();
