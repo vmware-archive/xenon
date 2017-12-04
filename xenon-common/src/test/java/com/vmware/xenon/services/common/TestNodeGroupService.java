aac1921a1e5480addb1101a513d93dc25de71b50
Tadaya Tsuyukubo <ttsuyukubo@vmware.com>
2017-12-04 12:49:05 -0800

    Add auth to UtilityService
    Extend auth check to UtiliyService endpoints: stats, config, subscription
    
    Issue: VRXEN-5
    
    Change-Id: I52a4b4a42731c244a97f97610dcaddb6837e67fb

--- xenon-common/src/test/java/com/vmware/xenon/services/common/TestNodeGroupService.java
+++ xenon-common/src/test/java/com/vmware/xenon/services/common/TestNodeGroupService.java
@@ -2482,6 +2482,7 @@ public class TestNodeGroupService {
 
         VerificationHost groupHost = this.host.getPeerHost();
 
+        groupHost.setSystemAuthorizationContext();
         // wait for auth related services to be stabilized
         groupHost.waitForReplicatedFactoryServiceAvailable(
                 UriUtils.buildUri(groupHost, UserService.FACTORY_LINK));
@@ -2499,7 +2500,7 @@ public class TestNodeGroupService {
         String bazUserLink = UriUtils.buildUriPath(ServiceUriPaths.CORE_AUTHZ_USERS,
                 "baz@vmware.com");
 
-        groupHost.setSystemAuthorizationContext();
+
 
         // create user, user-group, resource-group, role for foo@vmware.com
         //   user: /core/authz/users/foo@vmware.com
@@ -2709,7 +2710,7 @@ public class TestNodeGroupService {
 
             // based on the role created in test, all users have access to ExampleService
             this.host.sendAndWaitExpectSuccess(
-                    Operation.createGet(UriUtils.buildStatsUri(peer, ExampleService.FACTORY_LINK)));
+                    Operation.createGet(UriUtils.buildUri(peer, ExampleService.FACTORY_LINK)));
         }
 
         this.host.waitFor("Timeout waiting for correct auth cache state",
