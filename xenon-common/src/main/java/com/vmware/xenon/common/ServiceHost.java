4366c81a7a63d59212850608ec69663918e7b0af
Tadaya Tsuyukubo <ttsuyukubo@vmware.com>
2018-02-28 10:38:15 -0800

    Fix auth with websocket with minimum AuthFilter change
    Make AuthorizationContext populated for websocket when auth token is provided
    
    Issue: VRXEN-66
    Change-Id: I84ab5ed6d9d81d8870c2fb7e27fd83adfdcc1ad1

--- xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
@@ -5301,6 +5301,30 @@ public class ServiceHost implements ServiceRequestSender {
     }
 
     /**
+     * Based on the auth token, populate AuthorisationContext and set it to OperationContext as well.
+     *
+     * The callback consumer will receive the populated auth context.
+     * When given token is not valid, it will populate guest context.
+     *
+     * Infrastructure use only.
+     *
+     * @param authToken    auth token
+     * @param referrerPath used as a referrer when verifying the given token
+     * @param callback     a callback operation.
+     */
+    public void populateAuthorizationContext(String authToken, String referrerPath, Consumer<AuthorizationContext> callback) {
+        Operation dummyOp = new Operation();
+        dummyOp.addRequestHeader(Operation.REQUEST_AUTH_TOKEN_HEADER, authToken);
+        dummyOp.setUri(UriUtils.buildUri(this, referrerPath));
+
+        OperationProcessingContext context = this.opProcessingChain.createContext(this);
+        this.authorizationFilter.populateAuthorizationContext(dummyOp, context, (authCtx) -> {
+            OperationContext.setAuthorizationContext(authCtx);
+            callback.accept(authCtx);
+        });
+    }
+
+    /**
      * Return the system user's authorization context.
      *
      * @return authorization context.
