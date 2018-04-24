52a2711ae467fbfeff4329ffafbcb049f147ed6f
Li Yu <lyu1@vmware.com>
2018-04-23 17:20:04 -0700

    Check convergence and update cached node state
    Synchronization could be avoid when one of node
    turns to be unavailable. However, it does not necessary
    mean node group converge. Additional check and update
    is required to update cached state in node selector
    and status of factory.
    
    Change-Id: I031e604363abfe1cb3141fa47277084a18ec0416

--- xenon-common/src/main/java/com/vmware/xenon/services/common/ConsistentHashingNodeSelectorService.java
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/ConsistentHashingNodeSelectorService.java
@@ -637,10 +637,6 @@ public class ConsistentHashingNodeSelectorService extends StatelessService imple
             return false;
         }
 
-        if (!this.isSynchronizationRequired && !this.isSetFactoriesAvailabilityRequired) {
-            return false;
-        }
-
         if (!NodeGroupUtils.isMembershipSettled(getHost(), getHost().getMaintenanceIntervalMicros(),
                 this.cachedGroupState)) {
             checkConvergence(membershipUpdateMicros, maintOp);
@@ -652,11 +648,7 @@ public class ConsistentHashingNodeSelectorService extends StatelessService imple
             return true;
         }
 
-        if (!getHost().isPeerSynchronizationEnabled()) {
-            return false;
-        }
-
-        if (this.isSynchronizationRequired) {
+        if (this.isSynchronizationRequired && getHost().isPeerSynchronizationEnabled()) {
             this.isSynchronizationRequired = false;
             if (isDefaultNodeSelector()) {
                 logInfo("Scheduling synchronization (%d nodes)", this.cachedGroupState.nodes.size());
